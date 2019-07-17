938029b31bb6dd4dc81ee82abb8a0b43d4eb42a7
unknown <nataliiasoroka.gm@gmail.com>
2019-07-17 14:00:55 +0300

    Updates

---
+++ precache-manifest.8f7af8cf75c6f1c121ac1ad4bc66263b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eff96ca7480840625259b069c7e45a0",
+    "url": "/alerts-homework/index.html"
+  },
+  {
+    "revision": "d844f6eaa9266f845a8b",
+    "url": "/alerts-homework/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "634deb1312c00b4b8c6a",
+    "url": "/alerts-homework/static/js/2.af32eb37.chunk.js"
+  },
+  {
+    "revision": "d844f6eaa9266f845a8b",
+    "url": "/alerts-homework/static/js/main.ebed02b8.chunk.js"
+  },
+  {
+    "revision": "af44ae6c1870848364f6",
+    "url": "/alerts-homework/static/js/runtime~main.2137df95.js"
+  }
+]);
